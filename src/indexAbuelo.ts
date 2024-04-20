790ca5d286596d3a234386f1225616305ac1663a
MilaChan7777 <camila.lerma1@u.icesi.edu.co>
2024-04-20 13:08:28 -0500

    Clase 11

--- src/indexAbuelo.ts
+++ src/indexAbuelo.ts
@@ -12,7 +12,7 @@ class AppContainer extends HTMLElement {
 			const data = await getPokemons(i);
 			console.log(data);
 			this.render(data);
-			//hola mundo jeje
+			//hola mundo jeje cómo tan
 		}
 	}
 
